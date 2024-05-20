33fa6e6b8bd9b82eec609e6c23a62687acca35bf
Gabor Szabo <szasza576@gmail.com>
2024-05-20 14:12:06 +0200

    Adding guiding messages

--- azureml/aml-patcher/storage-patcher.sh
+++ azureml/aml-patcher/storage-patcher.sh
@@ -3,12 +3,14 @@
 # Use the "crictlurl" environmental variable to optionally specify the download URL.
 
 # Install crictl
+echo "Downloading crictl util..."
 defaulturl="https://github.com/kubernetes-sigs/cri-tools/releases/download/v1.30.0/crictl-v1.30.0-linux-arm64.tar.gz"
 if [ -z ${crictlurl+x} ]; then crictlurl=$defaulturl; fi
 wget -O - $crictlurl | \
 tar -xz -C /usr/bin/
 
 # Configure crictl
+echo "Configuring crictl..."
 tee /etc/crictl.yaml<<EOF
 runtime-endpoint: unix:///run/containerd/containerd.sock
 image-endpoint: unix:///run/containerd/containerd.sock
@@ -18,7 +20,7 @@ EOF
 
 while (true); do
     containerid=$(crictl ps| grep storageinitializer-modeldata | awk '{print $1}')
-    if [[ -n ${containerid} ]]; then 
+    if [[ -n "${containerid}" ]]; then 
         echo "Patching found storagerinitializer container"
         crictl update --cpu-quota 200000 --cpu-period 200000 $containerid
     fi
